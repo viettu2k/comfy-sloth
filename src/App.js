0b2b0e60fb34fbf2de561a751a030d9bb1978d69
viettu2k <doanviettu2k@gmail.com>
2021-09-09 09:31:49 +0700

    complementing navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
-import styled from "styled-components";
 
 import {
   Home,
