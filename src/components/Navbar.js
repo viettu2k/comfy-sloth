4de40bad13a42e0b87eb87c0113931eb4489e72f
viettu2k <doanviettu2k@gmail.com>
2021-09-10 15:32:06 +0700

    implementing about page, checkout page, pageHero component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,6 +31,9 @@ const Nav = () => {
               </li>
             );
           })}
+          <li>
+            <Link to="/checkout">checkout</Link>
+          </li>
         </ul>
         <CartButtons />
       </div>
